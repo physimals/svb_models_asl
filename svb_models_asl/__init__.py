3a080f1028261cfa87e3f79f011ee804ceceb193
Martin Craig <martin.craig@eng.ox.ac.uk>
2020-03-06 15:01:15 +0000

    Import version and timestamp

--- svb_models_asl/__init__.py
+++ svb_models_asl/__init__.py
@@ -1,3 +1,14 @@
+try:
+    from ._version import __version__, __timestamp__
+except ImportError:
+    __version__ = "Unknown version"
+    __timestamp__ = "Unknown timestamp"
+
 from .aslnn import AslNNModel
 from .aslrest import AslRestModel
 
+__all__ = [
+    "AslNNModel",
+    "AslRestModel",
+    "__version__"
+]
